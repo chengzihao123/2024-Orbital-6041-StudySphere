5ef482abf24ca7d8536f3d9ba3b17563872ad1b1
chengzihao123 <czh152103@gmail.com>
2024-05-28 14:04:37 +0800

    skeleton

---
+++ src/components/LoginTopBar.tsx
@@ -0,0 +1,12 @@
+export default function LoginTopBar() {
+  return (
+    <div className="flex justify-end w-full h-10 bg-black rounded-sm pt-1 pr-7">
+      <h1 className="text-lg font-bold text-white mr-7 hover:text-xl cursor-pointer">
+        Login
+      </h1>
+      <h1 className="text-lg font-bold text-white hover:text-xl cursor-pointer">
+        sign up
+      </h1>
+    </div>
+  );
+}
