59885a857e8b5c5ab59f2a0716d1fb11014ceb8a
chengzihao123 <77613117+chengzihao123@users.noreply.github.com>
2024-05-31 00:01:58 +0800

    Added study mode feature

--- frontend/src/components/LoginTopBar.tsx
+++ frontend/src/components/LoginTopBar.tsx
@@ -1,6 +1,16 @@
+import Link from "next/link";
+
 export default function LoginTopBar() {
   return (
     <div className="flex justify-end w-full h-10 bg-black rounded-sm pt-1 pr-7">
+      <div className="w-full pl-10 pt-1">
+        <Link
+          href="/study"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded-full"
+        >
+          Button
+        </Link>
+      </div>
       <h1 className="text-lg font-bold text-white mr-7 hover:text-xl cursor-pointer">
         Login
       </h1>
