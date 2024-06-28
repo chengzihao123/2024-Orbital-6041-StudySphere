91547968bb42fcb20530fa5561046982f2542e91
tzuennn <tzuen2406@gmail.com>
2024-06-28 23:33:50 +0800

    unit test for sign up

---
+++ frontend/jest.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+};
+
+export default config;
